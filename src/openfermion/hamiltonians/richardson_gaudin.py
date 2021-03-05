1631af25289241082eef78bdf24dba84f00341f1
cvjjm <christian.gogolin@covestro.com>
2021-03-05 10:58:19 +0000

    fixed prefactors of Richardson Gaudin Hamiltonian added test fixed docstrings

--- src/openfermion/hamiltonians/richardson_gaudin.py
+++ src/openfermion/hamiltonians/richardson_gaudin.py
@@ -9,133 +9,115 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-"""Class and functions to store interaction operators."""
-import numpy
+"""This module constructs Hamiltonians of the Richardson Gaudin type.
+"""
 
 from openfermion.ops import QubitOperator
-from openfermion.ops.representations import (PolynomialTensor,
-                                             get_tensors_from_integrals)
-
-COEFFICIENT_TYPES = (int, float, complex)
+from openfermion.ops.representations import PolynomialTensor
 
 
 class RichardsonGaudin:
-    r"""Class for storing Richardson Gaudin hamiltonians within an equi-distant potential
-    constant coupling term g which can be either attractive or repulsive indicated by its sign.
+    r"""Richardson Gaudin model.
 
-    Note that the operators stored in this class take the form:
+    Class for storing and constructing Richardson Gaudin hamiltonians
+    combining an equi-distant potential ladder like potential per
+    qubit with a uniform coupling between any pair of
+    qubits with coupling strength g, which can be either attractive
+    (g<0) or repulsive (g>0).
 
-        .. math::
+    The operators represented by this class has the form:
 
-            constant + \sum_{p} p N_p +
-            g \sum_{p \neq q} P_p^\dagger P_q,
+    .. math::
+
+        H = \sum_{p=0} (p+1) N_p + g/2 \sum_{p < q} P_p^\dagger P_q,
 
     where
 
-        .. math::
+    .. math::
 
-            N_p = (1 - \sigma^Z_p)/2,
-            P_p = a_{i,\beta} a_{i,\alpha},
-            g = constant coupling term
-
-    Attributes:
-        constant: The constant offset.
-        g = constant coupling term
+        \begin{align}
+        N_p &= (1 - \sigma^Z_p)/2, \\
+        P_p &= a_{p,\beta} a_{p,\alpha} = S^{-} = \sigma^X - i \sigma^Y, \\
+        g &= constant coupling term
+        \end{align}
     """
-
     def __init__(self, g, n_qubits):
-        r"""
-        Initialize the DOCIHamiltonian class.
+        r"""Richardson Gaudin model on a given number of qubits.
 
         Args:
-            g: The coefficients of (:math:`h^{(r2)}_{p, q}`).
-                This is an n_qubits x n_qubits array of floats.
+            g (float): Coupling strength
+            n_qubits (int): Number of qubits
         """
-        self._n_qubits = n_qubits 
         self._g = g
+        self._n_qubits = n_qubits
 
     @property
     def hamiltonian(self):
-        """Return the QubitOperator representation of this DOCI Hamiltonian"""
-        return self.identity_part + self.z_part + self.xy_part
-
-    def xx_term(self, p, q):
-        """Returns the XX term on a single pair of qubits as a QubitOperator
-        Arguments:
-            p, q [int] -- qubit indices
-        Returns:
-            [QubitOperator] -- XX term on the chosen qubits.
+        """QubitOperator representation of the Hamiltonian.
         """
-        return QubitOperator("X" + str(p) + " X" + str(q), self._g / 2)
-
-    def yy_term(self, p, q):
-        """Returns the YY term on a single pair of qubits as a QubitOperator
-        Arguments:
-            p, q [int] -- qubit indices
-        Returns:
-            [QubitOperator] -- YY term on the chosen qubits.
-        """
-        return QubitOperator("Y" + str(p) + " Y" + str(q), self._g / 2)
-
-    def z_term(self, p):
-        """Returns the Z term on a single qubit as a QubitOperator
-        Arguments:
-            p [int] -- qubit index
-        Returns:
-            [QubitOperator] -- Z term on the chosen qubit.
-        """
-        return QubitOperator("Z" + str(p), p / 2)
+        return self.identity_part + self.z_part + self.xx_and_yy_part
 
     @property
     def identity_part(self):
-        """Returns identity term of this operator (i.e. trace-ful term)
-        in QubitOperator form.
+        """Identity part of the QubitOperator representation of the Hamiltonian.
         """
-        return QubitOperator(
-            (), (self._n_qubits+1)*self._n_qubits / 2 )
+        return QubitOperator((), (self._n_qubits+1)*self._n_qubits// 2 / 2)
 
     @property
     def xx_part(self):
-        """Returns the XX part of the QubitOperator representation of this
-        DOCIHamiltonian
+        """XX part of the QubitOperator representation of the Hamiltonian.
         """
         return sum([
-            self.xx_term(p, q)
+            QubitOperator("X" + str(p) + " X" + str(q), self._g / 2)
             for p in range(self.n_qubits)
             for q in range(p + 1, self.n_qubits)
         ])
 
     @property
     def yy_part(self):
-        """Returns the YY part of the QubitOperator representation of this
-        DOCIHamiltonian
+        """YY part of the QubitOperator representation of the Hamiltonian.
         """
         return sum([
-            self.yy_term(p, q)
+            QubitOperator("Y" + str(p) + " Y" + str(q), self._g / 2)
             for p in range(self.n_qubits)
             for q in range(p + 1, self.n_qubits)
         ])
 
     @property
-    def xy_part(self):
-        """Returns the XX+YY part of the QubitOperator representation of this
-        DOCIHamiltonian
+    def z_part(self):
+        """Z part of the QubitOperator representation of the Hamiltonian.
+        """
+        return sum([QubitOperator("Z" + str(p), (p+1)/2) for p in range(self.n_qubits)])
+
+    @property
+    def xx_and_yy_part(self):
+        """XX and YY parts combined of the QubitOperator representation of the Hamiltonian.
         """
         return self.xx_part + self.yy_part
 
     @property
-    def z_part(self):
-        """Return the Z and ZZ part of the QubitOperator representation of this
-        DOCI Hamiltonian"""
-        return sum([self.z_term(p) for p in range(self.n_qubits)])
-
-
-    @property
     def g(self):
-        """The value of g"""
+        """The coupling strength g.
+
+        Returns:
+            float: The coupling strength g.
+        """
         return self._g
 
     @g.setter
     def g(self, value):
+        """Sets the coupling strength g.
+
+        Args:
+            g (float): The coupling strength g.
+        """
         self._g = value
 
+    @property
+    def n_qubits(self):
+        """The numner of qubits.
+
+        Returns:
+            int: The number of qubits.
+        """
+        return self._n_qubits
---
+++ src/openfermion/hamiltonians/richardson_gaudin_test.py
@@ -0,0 +1,29 @@
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+"""Tests for Richardson Gaudin model module."""
+
+import pytest
+
+from openfermion.hamiltonians import RichardsonGaudin
+from openfermion.ops import QubitOperator
+
+@pytest.mark.parametrize(
+    'g, n_qubits, expected',
+    [
+        (0.3, 2, QubitOperator('1.5 [] + 0.15 [X0 X1] + 0.15 [Y0 Y1] + 0.5 [Z0] + 1.0 [Z1]')),
+        (-0.1, 3, QubitOperator('3.0 [] - 0.05 [X0 X1] - 0.05 [X0 X2] - 0.05 [Y0 Y1] - 0.05 [Y0 Y2] + 0.5 [Z0] - 0.05 [X1 X2] - 0.05 [Y1 Y2] + 1.0 [Z1] + 1.5 [Z2]')),
+    ]
+)
+def test_richardson_gaudin_hamiltonian(
+        g, n_qubits, expected):
+    op = RichardsonGaudin(g, n_qubits)
+    assert op.hamiltonian == expected
